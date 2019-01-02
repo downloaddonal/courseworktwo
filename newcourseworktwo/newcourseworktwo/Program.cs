6e5ed53745df7cc516fb718355022f518cdfdc0e
downloaddonal <34678118+downloaddonal@users.noreply.github.com>
2019-01-02 11:20:37 +0000

    updated

--- newcourseworktwo/newcourseworktwo/Program.cs
+++ newcourseworktwo/newcourseworktwo/Program.cs
@@ -1,74 +1,101 @@
 using System;
 
-namespace courseworkone
+namespace cw2_mk2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // creating variables doubles for sales,array to take the store names
-            // using a for loop to iterate over the question itself
+            // Candidate names and votes
+            int NumberOfCandidates, HighestVote;
+            double dubEligibleVoters;
+            string Valid, Winner;
+            string HighestCandidate;
+            double dubEligibleVotersPercent, dubTotalVotesPercent, dubWinningPercent, HighestVotePercent;
 
-            Double dubOverallQ1, dubOverallQ2, dubOverallQ3, dubOverallQ4,
-                dubTotals1, dubTotals2, dubTotals3, dubTotals4,
-                dubStore1Percent, dubStore2Percent, dubStore3Percent, dubStore4Percent,
-                dubGrandRotal;
+            // Variable candidate amount used to prep the arrays
+            Console.WriteLine("Please enter the number of candidates between 1-8: ");
+            NumberOfCandidates = Convert.ToInt32(Console.ReadLine());
 
-            // two arrays here, strings and a 2 dimensional one for the figures
-            String[] StoreNames = new string[4];
-            int[,] StoreSales = new int[4, 4];
+            string[] CandidateNames = new string[NumberOfCandidates];
+            double[] CandidateVotes = new double[NumberOfCandidates];
 
-            // for loop to iterate over the needed details
-            for (int i = 0; i < StoreNames.Length; i++)
+            //eligible voters overall
+            Console.WriteLine("How many eligible voters overall: ");
+            dubEligibleVoters = Convert.ToDouble(Console.ReadLine());
+
+            // inputing data to the arrays 
+            double Total = 0;
+            for (int i = 0; i < NumberOfCandidates; i++)
             {
-                Console.Write("\nPlease enter the name of Store: ");
-                StoreNames[i] = Console.ReadLine();
-                Console.Write("Please enter the quarterly sales for " + StoreNames[i] + " please enter the digits in thousands i.e 12,000 as 12 :\n");
-                Console.Write("Please enter the Q1 sales for " + StoreNames[i] + "\n");
-                StoreSales[i, 0] = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Please enter the Q2 sales for " + StoreNames[i] + "\n");
-                StoreSales[i, 1] = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Please enter the Q3 sales for " + StoreNames[i] + "\n");
-                StoreSales[i, 2] = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Please enter the Q4 sales for " + StoreNames[i] + "\n");
-                StoreSales[i, 3] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the name of candidate: ");
+                CandidateNames[i] = Console.ReadLine();
+                Console.WriteLine("How many votes for candidate: ");
+                CandidateVotes[i] = Convert.ToDouble(Console.ReadLine());
+                Total = Total + CandidateVotes[i];
             }
 
-            // now to calculate the totals for overal sales
-            // per store
+            // doing the percentages for the if statements
 
-            dubTotals1 = StoreSales[0, 0] + StoreSales[0, 1] + StoreSales[0, 2] + StoreSales[0, 3];
-            dubTotals2 = StoreSales[1, 0] + StoreSales[1, 1] + StoreSales[1, 2] + StoreSales[1, 3];
-            dubTotals3 = StoreSales[2, 0] + StoreSales[2, 1] + StoreSales[2, 2] + StoreSales[2, 3];
-            dubTotals4 = StoreSales[3, 0] + StoreSales[3, 1] + StoreSales[3, 2] + StoreSales[3, 3];
-            dubGrandTotal = dubTotals1 + dubTotals2 + dubTotals3 + dubTotals4;
+            dubEligibleVotersPercent = (dubEligibleVoters / dubEligibleVoters) * 60; // determine 60% of eligible for valid/invalid
+            dubTotalVotesPercent = (Total / dubEligibleVoters) * 100;
+            dubWinningPercent = (Total / Total) * 50; // total by total 50% of total votes to determine winner
 
-            // totals
-            // all stores
-            dubOverallQ1 = StoreSales[0, 0] + StoreSales[1, 0] + StoreSales[2, 0] + StoreSales[3, 0];
-            dubOverallQ2 = StoreSales[0, 1] + StoreSales[1, 1] + StoreSales[2, 1] + StoreSales[3, 1];
-            dubOverallQ3 = StoreSales[0, 2] + StoreSales[1, 2] + StoreSales[2, 2] + StoreSales[3, 2];
-            dubOverallQ4 = StoreSales[0, 3] + StoreSales[1, 3] + StoreSales[2, 3] + StoreSales[3, 3];
+            //if statement to figure out if election is valid 
+            if (dubTotalVotesPercent >= dubEligibleVotersPercent)
+            {
+                Valid = "The election was valid";
+                //find out the most vote percent
+                HighestVote = 0;
+                HighestCandidate = "";
+                for (int i = 0; i < NumberOfCandidates; i++)
+                {
+                    int currentvote = (int)CandidateVotes[i];
+                    // this if statement compares the candidate vote with the highest vote and when it does sets the highest scoring candidate
+                    if (CandidateVotes[i] > HighestVote)
+                    {
+                        HighestVote = currentvote;
+                        HighestCandidate = CandidateNames[i];
+                    }
 
-            // doing the percentage math bit
+                }
+                // takes the highest vote percent against the winning percent and determines if enough people voted to win
+                HighestVotePercent = (HighestVote / Total) * 100;
+                if (HighestVotePercent >= dubWinningPercent)
+                {
+                    Winner = "Enough people voted for " + HighestCandidate + " to win" + " with " + HighestVote + " votes";
+                }
+                else
+                {
+                    Winner = "Not Enough people voted " + HighestCandidate + " to win";
+                }
+                Console.WriteLine(Valid);
+                Console.WriteLine("The total amount of votes was " + Total);
+                Console.WriteLine(Winner);
+                Console.WriteLine("Candidate" + "\t" + "Votes");
 
-            dubStore1Percent = (dubTotals1 / dubGrandTotal) * 100;
-            dubStore2Percent = (dubTotals2 / dubGrandTotal) * 100;
-            dubStore3Percent = (dubTotals3 / dubGrandTotal) * 100;
-            dubStore4Percent = (dubTotals4 / dubGrandTotal) * 100;
+                // for loop to output the arrays as a list
+                for (int i = 0; i < NumberOfCandidates; i++)
+                {
+                    Console.WriteLine(CandidateNames[i] + "\t\t" + CandidateVotes[i]);
+                }
+                Console.ReadLine();
+            }
+            else
+            {
+                Valid = "The election was invalid";
+                Winner = "there was no winner due to invalid election";
+                Console.WriteLine("The total amount of votes was " + Total);
+                Console.WriteLine(Valid + "\n" + Winner);
+                Console.WriteLine("Candidate" + "\t" + "Votes");
 
-            // all variables have been set so we will now display the table
-
-            Console.WriteLine("\n");
-            Console.WriteLine("Sales figures: " + "\t" + StoreNames[0] + "\t" + StoreNames[1] + "\t" + StoreNames[2] + "\t" + StoreNames[3] + "\t" + "Overall" + "\n");
-            Console.WriteLine("Quarter one: " + "\t" + StoreSales[0, 0] + "\t" + StoreSales[1, 0] + "\t" + StoreSales[2, 0] + "\t" + StoreSales[3, 0] + "\t" + dubOverallQ1 + "\n");
-            Console.WriteLine("Quarter two: " + "\t" + StoreSales[0, 1] + "\t" + StoreSales[1, 1] + "\t" + StoreSales[2, 1] + "\t" + StoreSales[3, 1] + "\t" + dubOverallQ2 + "\n");
-            Console.WriteLine("Quarter three: " + "\t" + StoreSales[0, 2] + "\t" + StoreSales[1, 2] + "\t" + StoreSales[2, 2] + "\t" + StoreSales[3, 2] + "\t" + dubOverallQ3 + "\n");
-            Console.WriteLine("Quarter four: " + "\t" + StoreSales[0, 3] + "\t" + StoreSales[1, 3] + "\t" + StoreSales[2, 3] + "\t" + StoreSales[3, 3] + "\t" + dubOverallQ4 + "\n");
-            Console.WriteLine("Total: " + "\t\t" + dubTotals1 + "\t" + dubTotals2 + "\t" + dubTotals3 + "\t" + dubTotals4 + "\t" + dubGrandTotal);
-            Console.WriteLine("\n");
-            Console.WriteLine(" %  Overall: " + "\t" + StoreNames[0] + "\t" + StoreNames[1] + "\t" + StoreNames[2] + "\t" + StoreNames[3]);
-            Console.WriteLine("\t\t" + Math.Round(dubStore1Percent, 1) + "\t" + Math.Round(dubStore2Percent, 1) + "\t" + Math.Round(dubStore3Percent, 1) + "\t" + Math.Round(dubStore4Percent, 1));
-            Console.ReadLine();
+                // for loop to output the arrays as a list
+                for (int i = 0; i < NumberOfCandidates; i++)
+                {
+                    Console.WriteLine(CandidateNames[i] + "\t\t" + CandidateVotes[i]);
+                }
+                Console.ReadLine();
+            }
         }
     }
+}
